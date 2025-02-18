d448617c89cf75fd98097ea92e8e444d97c873d0
vegetats <daramarotto4@gmail.com>
2025-02-18 16:25:20 -0300

    feat✨

--- scripts.js
+++ scripts.js
@@ -5,51 +5,120 @@ const cartHtml = document.getElementById('products-on-cart');
 const divItemsCart = document.getElementById('items-cart');
 const newUl = document.createElement('ul');
 divItemsCart.appendChild(newUl);
-const allIndividualValues = [];
-let individualValue = 0; 
-let totalValue = 0;
+let newLi;
+
+
+let item;
 
 const cartItems = []; 
-let foundItem = {};
+let selectedItem = {};
 
 const options  = [
-    {value : 'item1', name : 'Air Pods 4', price : 10},
-    {value : 'item2', name : 'MackBook Pro', price : 20},
-    {value : 'item3', name : 'iPad Pro', price : 30},
-    {value : 'item4', name : 'iPhone 15 Pro', price : 40}
+    {id : 1, name : 'Air Pods 4', price : 10},
+    {id : 2, name : 'MackBook Pro', price : 20},
+    {id : 3, name : 'iPad Pro', price : 30},
+    {id : 4, name : 'iPhone 15 Pro', price : 40}
 ];
 
 
 function returnObjectItem() {
-    selectElementHtml.addEventListener('change', function() {
+    selectElementHtml.addEventListener('change', function() { // * consertar
   
-        const selectedItem = selectElementHtml.value;
+        item = selectElementHtml.value;
       
-        foundItem = options.find(option => option.value === selectedItem);
+        selectedItem = options.find(option => option.id == item);
     });
 }
 
 returnObjectItem();
 
 
+let currentQuantity; 
+let additionalQuantity; 
+const allIndividualValues = []; 
 
 function addToCart() {
+    const selectedItemInDom = document.getElementById(selectedItem.id);
+    if(selectedItemInDom) {
     
-    cartItems.push({
-        quantity: inputQuantityHtml.value,
-        name: foundItem.name,
-        price: foundItem.price,
-        value : foundItem.value
-    });
-    
-    const newLi = document.createElement('li');
-    newLi.textContent = `${inputQuantityHtml.value}  ${foundItem.name}`; 
-    newLi.setAttribute('id', foundItem.value);
-    newUl.appendChild(newLi);
+        additionalQuantity = Number(inputQuantityHtml.value) || 0;
+        currentQuantity = Number(selectedItemInDom.textContent.split(' ')[0]); 
+        
+        let valuesForSum = (additionalQuantity * selectedItem.price); 
+        allIndividualValues.push({
+            price : valuesForSum,
+            id : selectedItem.id,
+            quantity : (additionalQuantity + currentQuantity)
+        });
+        
+        selectedItemInDom.textContent = `${currentQuantity + additionalQuantity} ${selectedItem.name}`;
+        const existingItem = cartItems.find(item => item.id == selectedItem.id);
+        if(existingItem) existingItem.quantity += selectedItem.quantity; 
 
-    individualValue = (inputQuantityHtml.value * foundItem.price);
-    allIndividualValues.push(individualValue);
-    totalValue = allIndividualValues.reduce((total, num) => total + num, 0); 
-    totalValueHtml.textContent = `R$ ${totalValue}`;  
+
+        createButtonRemoveItems();
+
+    } else {
+        cartItems.push({
+            quantity: inputQuantityHtml.value,
+            name: selectedItem.name,
+            price: selectedItem.price,
+            id : selectedItem.id
+        });
+    
+        newLi = document.createElement('li');
+        newLi.textContent = `${inputQuantityHtml.value}  ${selectedItem.name}`; 
+        newLi.setAttribute('id', selectedItem.id);
+        newUl.appendChild(newLi);
+        createButtonRemoveItems();
+        
+    
+        valuesForSum = selectedItem.price * inputQuantityHtml.value; 
+        allIndividualValues.push({
+            price : valuesForSum,
+            id : selectedItem.id,
+            quantity : 1
+        });
+    }
+    
+    calculateTotalValue(); 
+   
 }
+
+let total = 0;
+function calculateTotalValue() {
+    
+    total = allIndividualValues.reduce((acc, item) => acc + item.price, 0); 
+    totalValueHtml.textContent = `Valor Total R$ ${total}`;
+
+}
+
+function createButtonRemoveItems() {
+    const buttonRemoveItem = document.createElement('button');
+    buttonRemoveItem.textContent = 'Remover un';
+    newLi.appendChild(buttonRemoveItem); 
+    buttonRemoveItem.onclick = function () { 
+        // allIndividualValues.filter(item => item.id === selectedItem.id)
+        // .map(allIndividualValues = {
+        //     price : valuesForSum,
+        //     id : selectedItem.id,
+        //     quantity : 
+        // })
+
+    }
+          
+}
+
+
+
+
+
+    // const buttonRemoveLi = document.createElement('button');
+    // buttonRemoveLi.textContent = 'Remover todos';
+    // newLi.appendChild(buttonRemoveLi);
+    // buttonRemoveLi.onclick = function () {
+
+    // }
+
+
 
